c5b43ccb4f70359fece364871f3ff246c4afae24
Jonas Lindberg Frellesen <jolf@kb.dk>
2018-04-20 10:03:40 +0200

    Finished implementing the transfer to pre-ingest, along with update. Needs tests etc.

--- src/main/java/dk/kb/elivagar/transfer/PreIngestTransfer.java
+++ src/main/java/dk/kb/elivagar/transfer/PreIngestTransfer.java
@@ -3,8 +3,10 @@ package dk.kb.elivagar.transfer;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -25,6 +27,8 @@ import dk.kb.elivagar.utils.FileUtils;
 
 /**
  * Class for dealing with the transfer of the packaged data to the pre-ingest area of the preservation repository.
+ * 
+ * 
  */
 public class PreIngestTransfer {
     /** The logger.*/
@@ -34,6 +38,13 @@ public class PreIngestTransfer {
     protected static final String XPATH_PUBLICATION_DATE = "/Book/PublicationDate/text()";
     /** The date format for the publication date in the pubhub metadata file.*/
     protected static final String DATE_FORMAT_PUBLICATION_DATE = "DD-MM-YYYY";
+    
+    /** The list of suffixes of the metadata files, which should be updated at the metadata destination.*/
+    protected static final List<String> UPDATE_METADATA_SUFFIXES = Arrays.asList(Constants.PUBHUB_METADATA_SUFFIX,
+            Constants.MODS_METADATA_SUFFIX);
+    /** The list of suffixes of technical metadata files which should be update along the content destination.*/
+    protected static final List<String> UPDATE_TECH_METADATA_SUFFIXES = Arrays.asList(
+            Constants.EPUBCHECK_METADATA_SUFFIX, Constants.FITS_METADATA_SUFFIX);
 
     /** The configuration.*/
     protected final Configuration conf;
@@ -95,16 +106,55 @@ public class PreIngestTransfer {
             log.error("Failure while transfering books from '" + dir + "'", e);
         }
     }
-
-    public void updateBook(File bookDir, TransferRegistry register) {
+    
+    /**
+     * Update a book.
+     * Will try to find any metadata, technical metadata or content files which are newer than the latest
+     * update (or the ingest date, if it has not yet been updated).
+     * New files will be copied to their designated update directory, and it will be registered 
+     * that a new update has occurred.
+     * @param bookDir The directory of the book.
+     * @param register The register for the book.
+     */
+    protected void updateBook(File bookDir, TransferRegistry register) throws IOException {
         Date updateDate = register.getLatestUpdateDate();
         if(updateDate == null) {
             log.warn("Cannot retrieve neither update date nor ingest date from the registry. ");
             return;
         }
+        boolean updated = false;
 
-        // TODO: Find out whether symbolic links have their own dates 
-        // TODO: find any new files.
+        // Check for any metadata to update
+        List<File> metadataFiles = getNewFilesWithSuffix(bookDir, UPDATE_METADATA_SUFFIXES, updateDate);
+        if(!metadataFiles.isEmpty()) {
+            log.info("Found new metadata files for update.");
+            File updateDir = getUpdateMetadataDir(bookDir);
+            copyToUpdateDir(updateDir, metadataFiles);
+            updated = true;
+        }
+        
+        // Check for any technical metadata to update
+        List<File> techMetadataFiles = getNewFilesWithSuffix(bookDir, UPDATE_TECH_METADATA_SUFFIXES, updateDate);
+        if(!techMetadataFiles.isEmpty()) {
+            log.info("Found new technical metadata files for update.");
+            File updateDir = getUpdateContentDir(bookDir);
+            copyToUpdateDir(updateDir, techMetadataFiles);
+            updated = true;
+        }
+
+        // Check for any content files to update.
+        List<File> contentFiles = getNewContentFiles(bookDir, updateDate);
+        if(!contentFiles.isEmpty()) {
+            log.info("Found new content files for update.");
+            File updateDir = getUpdateContentDir(bookDir);
+            copyToUpdateDir(updateDir, contentFiles);
+            updated = true;            
+        }        
+        
+        if(updated) {
+            log.debug("Setting the new update date in the register for book '" + bookDir.getName() + "'");
+            register.setUpdateDate(new Date());
+        }
     }
 
     /**
@@ -113,7 +163,7 @@ public class PreIngestTransfer {
      * @param register The register for the book.
      * @throws IOException If it fails to transfer the book.
      */
-    public void ingestBook(File bookDir, TransferRegistry register) throws IOException {
+    protected void ingestBook(File bookDir, TransferRegistry register) throws IOException {
         if(readyForIngest(bookDir)) {
             File outputDir = new File(conf.getTransferConfiguration().getIngestDir(), bookDir.getName());
             FileUtils.copyDirectory(bookDir, outputDir);
@@ -142,8 +192,8 @@ public class PreIngestTransfer {
 
         // check content file date
         try {
-            List<File> contentFiles = getContentFiles(bookDir);
-            for(File f : contentFiles) {
+            List<Path> contentFiles = getContentFiles(bookDir);
+            for(Path f : contentFiles) {
                 if(!hasContentFileDate(f)) {
                     log.debug("Content file is too new.");
                     return false;
@@ -191,16 +241,51 @@ public class PreIngestTransfer {
         }
         return false;
     }
+    
+    /**
+     * Retrieves the directory for updating the metadata for the given book.
+     * @param bookDir The current directory of the book.
+     * @return The update metadata directory for the book.
+     * @throws IOException If it fails to create/retrieve the directory.
+     */
+    protected File getUpdateMetadataDir(File bookDir) throws IOException {
+        File updateDir = new File(conf.getTransferConfiguration().getUpdateMetadataDir(), bookDir.getName());
+        return FileUtils.createDirectory(updateDir.getAbsolutePath());
+    }
+    
+    /**
+     * Retrieves the directory for updating the content and technical metadata for the given book.
+     * @param bookDir The current directory of the book.
+     * @return The update content directory for the book.
+     * @throws IOException If it fails to create/retrieve the directory.
+     */
+    protected File getUpdateContentDir(File bookDir) throws IOException {
+        File updateDir = new File(conf.getTransferConfiguration().getUpdateContentDir(), bookDir.getName());
+        return FileUtils.createDirectory(updateDir.getAbsolutePath());
+    }
+    
+    /**
+     * Copies all the files to the update directory.
+     * @param updateDir The directory where the files should be copied to.
+     * @param files The file to update.
+     * @throws IOException If they fail to copy the files.
+     */
+    protected void copyToUpdateDir(File updateDir, List<File> files) throws IOException {
+        for(File fromFile : files) {
+            File toFile = new File(updateDir, fromFile.getName());
+            FileUtils.copyFileFollowSymbolicLinks(fromFile, toFile);
+        }
+    }
 
     /**
      * Check whether the given file has the right dates, both create-date and last-modify-date.
      * 
-     * @param f The file to check.
+     * @param path The path to the file to check.
      * @return Whether or not it has a new enough date.
      * @throws IOException If it fails to ready the system timestamps on the file.
      */
-    protected boolean hasContentFileDate(File f) throws IOException {
-        BasicFileAttributes attributes = Files.readAttributes(f.toPath(), BasicFileAttributes.class);
+    protected boolean hasContentFileDate(Path path) throws IOException {
+        BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
         
         if(conf.getTransferConfiguration().getRetainCreateDate() > 0) {
             long earliestDate = System.currentTimeMillis() - conf.getTransferConfiguration().getRetainCreateDate();
@@ -239,20 +324,20 @@ public class PreIngestTransfer {
     }
 
     /**
-     * Retrieves the content files for the given book.
+     * Retrieves the path to the content files for the given book.
      * It will most times only give one file, but occasionally there will be multiple content files.
      * @param bookDir The directory with the files.
-     * @return The content files for the book.
+     * @return The path to the content files for the book.
      * @throws IOException If it fails to follow the symbolic links to the content files.
      */
-    protected List<File> getContentFiles(File bookDir) throws IOException {
-        List<File> res = new ArrayList<File>();
+    protected List<Path> getContentFiles(File bookDir) throws IOException {
+        List<Path> res = new ArrayList<Path>();
 
         // Find books with Ebook suffixes.
         for(String format : conf.getEbookFormats()) {
             File f = new File(bookDir, bookDir.getName() + "." + format);
             if(f.exists()) {
-                res.add(Files.readSymbolicLink(f.toPath()).toFile());
+                res.add(Files.readSymbolicLink(f.toPath()));
             }
         }
 
@@ -260,10 +345,50 @@ public class PreIngestTransfer {
         for(String format : conf.getAudioFormats()) {
             File f = new File(bookDir, bookDir.getName() + "." + format);
             if(f.exists()) {
-                res.add(Files.readSymbolicLink(f.toPath()).toFile());
+                res.add(Files.readSymbolicLink(f.toPath()));
             }
         }
 
+        return res;
+    }
+    
+    /**
+     * Retrieves the files from the directory with the given suffixes and which have a newer last modified
+     * timestamp than a given limit.
+     * @param bookDir The directory for the book.
+     * @param suffixes The suffixes to find.
+     * @param lastModifiedLimit The 
+     * @return The list of new files with the given suffixes.
+     */
+    protected List<File> getNewFilesWithSuffix(File bookDir, List<String> suffixes, Date lastModifiedLimit) {
+        List<File> res = new ArrayList<File>();
+        for(String suffix : suffixes) {
+            for(File f : bookDir.listFiles()) {
+                if(f.getName().endsWith(suffix)) {
+                    if(f.lastModified() > lastModifiedLimit.getTime()) {
+                        res.add(f);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    
+    /**
+     * Retrieves the new content files from the given book directory.
+     * @param bookDir The book directory.
+     * @param lastModifiedLimit The lower limit for the last modified timestamp.
+     * @return The list of content files, which are newer than the last modified timestamp limit.
+     * @throws IOException If it fails to find the content files (through symlinks). 
+     */
+    protected List<File> getNewContentFiles(File bookDir, Date lastModifiedLimit) throws IOException {
+        List<Path> contentFilePaths = getContentFiles(bookDir);
+        List<File> res = new ArrayList<File>();
+        for(Path p : contentFilePaths) {
+            if(p.toFile().lastModified() > lastModifiedLimit.getTime()) {
+                res.add(p.toFile());
+            }
+        }
         return res;
     }
 }
